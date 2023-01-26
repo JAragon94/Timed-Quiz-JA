28ae9748e41035b56e3c638df614035195a2b60e
Josue Aragon <josue.aragon347@gmail.com>
2023-01-25 23:28:55 -0500

    Adjusted time to 60 secounds

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,7 @@ var questionIndex = 0;
 var totalTime = 151;
 function newQuiz() {
     questionIndex = 0;
-    totalTime = 999;
+    totalTime = 60;
     timeLeft.textContent = totalTime;
     initialInput.textContent = "";
 
